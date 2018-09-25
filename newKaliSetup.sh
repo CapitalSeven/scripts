509e7bf9ec39ef67bf9d6fd8a829f4e3bdbdda10
root <root@localhost.localdomain>
2018-09-24 22:33:35 -0400

    Added tgts.txt for hackthebox IP addresses.

--- newKaliSetup.sh
+++ newKaliSetup.sh
@@ -1,10 +1,11 @@
 echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-echo "[+] apt sources.list file updated."
+printf "[+] apt sources.list file updated. \n"
+printf "Hit enter and go get a coffee. \n This takes a while... \n"
 read -p "[+] Press enter to continue..."
 apt-get update -y && apt-get upgrade -y
 apt-get dist-upgrade
-echo "[+] Distribution up to date."
-read-p "[-] Install your virtualization tool suite manually, y\' bum."
+printf "[+] Distribution up to date. \n"
+read -p "[+] Press enter to continue..."
 ## TODO ##
 # detect hypervisor and install appropriate tools/guest-additions
 # install terminator >> import saved layout file?
